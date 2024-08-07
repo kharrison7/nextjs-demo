bd0418e18786ed22b7defc7fd40fc7611c310f4a
Kevin Harrison <kharrison7@elon.edu>
2024-08-07 10:19:59 -0400

    JRS-9703 updates package for react hook form and zod - also sets up basic login form page

--- app/formPage/page.tsx
+++ app/formPage/page.tsx
@@ -1,37 +1,56 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+// import { zodResolver } from "@hookform/resolvers/zod";
+// Need to run the FormFields: https://github.com/vercel/next.js/discussions/59483
+'use client'
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 
-export default function FormPage() {
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type FormFields = z.infer<typeof schema>;
+
+const App = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<FormFields>({
+    defaultValues: {
+      email: "test@email.com",
+    },
+    // resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<FormFields> = async (data: any) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log(data);
+    } catch (error) {
+      setError("root", {
+        message: "This email is already taken",
+      });
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        {/* // Sets up style for box: https://nextjs.org/learn/dashboard-app/css-styling */}
-        <div
-          className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
-        />
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>FORM PAGE.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              FORM PAGE
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-        </div>
-      </div>
-    </main>
+    <form className="tutorial gap-2" onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email")} type="text" placeholder="Email" />
+      {errors.email && (
+        <div className="text-red-500">{errors.email.message}</div>
+      )}
+      <input {...register("password")} type="password" placeholder="Password" />
+      {errors.password && (
+        <div className="text-red-500">{errors.password.message}</div>
+      )}
+      <button disabled={isSubmitting} type="submit">
+        {isSubmitting ? "Loading..." : "Submit"}
+      </button>
+      {errors.root && <div className="text-red-500">{errors.root.message}</div>}
+    </form>
   );
-}
+};
+
+export default App;
